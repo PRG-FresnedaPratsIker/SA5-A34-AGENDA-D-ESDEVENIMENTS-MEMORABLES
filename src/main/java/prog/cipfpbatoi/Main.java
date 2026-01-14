d599639d49dcb8a9f6b41ecb09a3bcaa76df14cf
Iker2102 <ikerfres21@gmail.com>
2026-01-14 16:35:45 +0000

    [NEW] Main

---
+++ src/main/java/prog/cipfpbatoi/Main.java
@@ -0,0 +1,13 @@
+package prog.cipfpbatoi;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Menu menu = new Menu();
+
+        menu.iniciar(); //no oblides tancar l’objecte de Scanner, dins d’aquest mètode
+
+    }
+
+}
